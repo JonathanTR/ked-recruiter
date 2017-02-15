63a50984838af175be19e879c8f3a33d248a1876
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-02-14 23:45:53 -0500

    Add zip code finder.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load files from lib folder
+Rails.application.configure do
+	config.autoload_paths += Dir["#{config.root}/lib/**/"]
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
