1622c8141e1e8bf4cd7f053c88da2e4c5b7ffca5
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-02-16 14:56:06 -0500

    Add tests and account for error states in ActionNetworkClient.

--- app/models/action_network_client.rb
+++ app/models/action_network_client.rb
@@ -1,51 +1,67 @@
 class ActionNetworkClient
-	def get_people(zip, radius)
-		nearby_zips = ZipCode.near(zip, radius)
-		response = conn.get('api/v2/people') do |request|
-			request.headers['ContentType'] = 'application/json'
-			request.headers['OSDI-API-Token'] = api_key
-			request.params = {
-				filter: "postal_code eq #{nearby_zips.join(' or ')}"
-			}
-		end
-		normalize_response(JSON.parse(response.body))
-	end
+  extend ActiveModel::Naming
+  attr_reader :errors
+
+  def initialize
+    @errors = ActiveModel::Errors.new(self)
+  end
+
+  def get
+    zip, radius = ['02122', 2]
+    get_people(zip, radius)
+  end
+
+  def get_people(zip, radius)
+    nearby_zips = ZipCode.near(zip, radius)
+    response = conn.get('api/v2/people') do |request|
+      request.headers['ContentType'] = 'application/json'
+      request.headers['OSDI-API-Token'] = api_key
+      request.params = { filter: "postal_code eq #{nearby_zips.join(' or')}" }
+    end
+    if response.success?
+      { status: 200, people: normalize_response(JSON.parse(response.body)) }
+    elsif !!(response.body[0..10] =~ /^<!DOCTYPE/)
+      { status: response.status, error: response.reason_phrase }
+    else
+      { status: response.status, error: JSON.parse(response.body)['error'] }
+    end
+  end
 
 
-	def normalize_response(response)
-		list = response['_embedded']['osdi:people']
-		list.map{|details| normalize_person(details)}
-	end
+  def normalize_response(response)
+    list = response['_embedded']['osdi:people']
+    list.map{|details| normalize_person(details)}
+  end
 
-	def normalize_person(hash)
-		person = hash.deep_symbolize_keys
-		{
-			given_name:       person[:given_name],
-			family_name:      person[:family_name],
-			email_address:    primary_email_for(person),
-			phone_number:     person[:custom_fields][:"Mobile number"],
-			address:          person[:postal_addresses],
-			languages_spoken: person[:languages_spoken],
-		}
-	end
+  def normalize_person(hash)
+    person = hash.deep_symbolize_keys
+    {
+      given_name:       person[:given_name],
+      family_name:      person[:family_name],
+      email_address:    primary_email_for(person),
+      phone_number:     person[:custom_fields][:"Mobile number"],
+      address:          person[:postal_addresses],
+      languages_spoken: person[:languages_spoken],
+    }
+  end
 
-	def primary_email_for(symbolized_person)
-		addresses = symbolized_person[:email_addresses]
-		if primary = addresses.find{|address| address[:primary]}
-			primary[:address]
-		else
-			nil
-		end
-	end
+  def primary_email_for(symbolized_person)
+    addresses = symbolized_person[:email_addresses]
+    if primary = addresses.find{|address| address[:primary]}
+      primary[:address]
+    else
+      nil
+    end
+  end
 
 
-	private
+  private
 
-	def api_key
-		Rails.application.secrets.action_network_api_key
-	end
+  def api_key
+    Rails.application.secrets.action_network_api_key
+  end
 
-	def conn
-		Faraday.new(url: 'https://actionnetwork.org/')
-	end
+  def conn
+    Faraday.new(url: 'https://actionnetwork.org/')
+  end
 end
--- test/models/action_network_client_test.rb
+++ test/models/action_network_client_test.rb
@@ -1,27 +1,60 @@
 require 'test_helper'
 
 class ActionNetworkClientTest < ActiveSupport::TestCase
-  setup do
-    stub_action_network_success
-    @client = ActionNetworkClient.new
-    @people = @client.get_people
-  end
-
   test '#get_people returns an array of people with contact details' do
-    assert_equal(@people.class, Array)
-    @people.each do |person|
+    stub_action_network_success
+    client = ActionNetworkClient.new
+    response = client.get_people('02122', 2)
+
+    assert_equal(response[:people].class, Array)
+    response[:people].each do |person|
       assert_includes(person, :given_name)
       assert_includes(person, :family_name)
       assert_includes(person, :email_address)
       assert_includes(person, :phone_number)
     end
   end
+
+  test '#get_people handles a JSON error from ActionNetwork' do
+    stub_action_network_400_error
+    client = ActionNetworkClient.new
+    response = client.get_people('02122', 2)
+
+    expected = 'API Key invalid or not present abdefg'
+    actual = response[:error]
+    assert_equal(expected, actual)
+  end
+
+  test '#get_people handles an HTML error from ActionNetwork' do
+    stub_action_network_500_error
+    client = ActionNetworkClient.new
+    response = client.get_people('02122', 2)
+
+    expected = 'Internal Server Error'
+    actual = response[:error]
+    assert_equal(expected, actual)
+  end
 end
 
 def stub_action_network_success
   success_fixture = file_fixture('action_network_people_sucess.json')
-  stub_request(:get, 'https://actionnetwork.org/api/v2/people').to_return({
+  stub_request(:get, /https:\/\/actionnetwork.org\/api\/v2\/people.+/).to_return({
     status: 200,
     body: success_fixture
+  })
+end
+
+def stub_action_network_400_error
+  stub_request(:get, /https:\/\/actionnetwork.org\/api\/v2\/people.+/).to_return({
+    status: 403,
+    body: {"error"=>"API Key invalid or not present abdefg"}.to_json
+  })
+end
+
+def stub_action_network_500_error
+  error_fixture = file_fixture('action_network_people_error_500.html')
+  stub_request(:get, /https:\/\/actionnetwork.org\/api\/v2\/people.+/).to_return({
+    status: [500, 'Internal Server Error'],
+    body: error_fixture,
   })
 end
