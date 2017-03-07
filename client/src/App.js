da215379d116c04875b3ab73eb0651b1ee318ac2
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-03-07 11:30:14 -0500

    Add initial test for rendering contacts.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contacts: []
+  };
+
   render() {
+    const { contacts } = this.state;
+
     return (
       <div className="App">
         <h1>#KNOCKEVERYDOOR</h1>
-        <input data-test='zipInput' placeholder='Enter your zipcode' />
+        <form data-test='zipInput'>
+          <input placeholder='Enter your zipcode' />
+          <button type='submit'>submit</button>
+        </form>
+        {contacts.length > 0 ?
+          contacts.map((contact, idx) => {
+            return(
+              <div data-test='contact' key={idx}>
+                {contact}
+              </div>
+            )
+          })
+        : null}
       </div>
     );
   }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -11,6 +11,18 @@ it('renders without crashing', () => {
 it('renders an input for a zip code', () => {
   const app = shallow(<App />);
   const zipInputs = app.find("[data-test='zipInput']");
-  expect(zipInputs.length).toEqual(1);
+
+  const actual = zipInputs.length;
+  const expected = 1;
+  expect(actual).toEqual(expected);
 });
 
+it('renders a list of contacts, if there are any', () => {
+  const app = shallow(<App />);
+  app.setState({contacts: ['one','two']});
+  const contacts = app.find("[data-test='contact']");
+
+  const actual = contacts.length;
+  const expected = 2
+  expect(actual).toEqual(expected);
+});
