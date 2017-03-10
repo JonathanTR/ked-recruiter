3c26e1c0d2cb05d2b1da56b1b47e66269eefdc68
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-03-09 23:27:28 -0500

    Add validations for people params, with tests.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,8 @@
 class PeopleController < ApplicationController
   before_action :set_person, only: [:update]
+  before_action :validate_params, only: [:index]
+  before_action :validate_zip, only: [:index]
+  before_action :validate_radius, only: [:index]
 
   def index
     client = ActionNetworkClient.new
@@ -20,12 +23,35 @@ class PeopleController < ApplicationController
   private
     def set_person
       if params[:action_network_id].blank?
-        render json: { error: "Missing parameter: 'action_network_id'"}
+        render json: { error: "Missing parameter: 'action_network_id'" },
+               status: :bad_request
       end
       @person = Person.find_by(action_network_id: params[:action_network_id])
     end
 
-    def fetch_and_sync_records(client, zips, list = [], page = 1, limit = 10)
+    def validate_params
+      [:zip, :radius].each do |param|
+        if params[param].blank?
+          render json: { error: "Missing parameter: '#{param}'" },
+                 status: :bad_request
+        end
+      end
+    end
+
+    def validate_zip
+      if ZipCode.find_by(code: params[:zip]).blank?
+        render json: { error: "Sorry, #{params[:zip]} is not a real zip code 😿" },
+               status: :bad_request
+      end
+    end
+
+    def validate_radius
+      if !params[:radius].match(/\d{1,3}/)
+        render json: { error: "Sorry, #{params[:radius]} is not a valid radius 😿" },
+               status: :bad_request
+      end
+    end
+
       response = client.request_people(zips, page)
       return response if response[:error]
       return {people: list} if response[:people].empty?
--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
+require 'lib/action_network_stubs'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
+  include ActionNetworkStubs
+
   setup do
     @person = people(:one)
   end
@@ -23,5 +26,33 @@ class PeopleControllerTest < ActionDispatch::IntegrationTest
     }, as: :json
     record = Person.find_by(action_network_id: @person.action_network_id)
     assert_equal(record.reload.available?, false)
+  end
+
+  test "validates presence of zip code" do
+    stub_action_network_success
+    get people_url(params: {radius: 5})
+    body = JSON.parse(response.body)
+    assert_not_nil(body['error'])
+  end
+
+  test "validates presence of a radius" do
+    stub_action_network_success
+    get people_url(params: {zip: '02122'})
+    body = JSON.parse(response.body)
+    assert_not_nil(body['error'])
+  end
+
+  test "validates format of zip" do
+    stub_action_network_success
+    get people_url(params: {zip: '99999', radius: 25})
+    body = JSON.parse(response.body)
+    assert_includes(body['error'], 'not a real zip code')
+  end
+
+  test "validates format of radius" do
+    stub_action_network_success
+    get people_url(params: {zip: '02122', radius: 'abc'})
+    body = JSON.parse(response.body)
+    assert_includes(body['error'], 'not a valid radius')
   end
 end
