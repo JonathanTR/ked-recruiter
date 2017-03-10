6a375f59b61d021ce97176e70a5c5ccc730c0dd1
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-03-09 20:09:54 -0500

    Rely on presence of error key to determine whether to render error.

--- client/src/api/Client.js
+++ client/src/api/Client.js
@@ -2,19 +2,8 @@ function getPeople(code, callback) {
   return fetch(`http://localhost:3000/people?zip=${code}&radius=25`, {
     accept: 'application/json',
   })
-  .then(checkStatus)
   .then(parseJSON)
-  .then(callback);
-}
-
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  const error = new Error(`HTTP Error ${response.statusText}`);
-  error.status = response.statusText;
-  error.response = response;
-  throw error;
+  .then(callback)
 }
 
 function parseJSON(response) {
