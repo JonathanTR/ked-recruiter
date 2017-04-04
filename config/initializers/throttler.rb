649ee72b9adfadeb75abec96b2964f15971b5a51
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-04-03 22:33:18 -0400

    Use non-deprecated keyword.

--- config/initializers/throttler.rb
+++ config/initializers/throttler.rb
@@ -4,7 +4,7 @@ class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
   # Whitelist localhost from throttling
-  whitelist('allow-localhost') do |req|
+  safelist('allow-localhost') do |req|
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
 
