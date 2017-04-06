92418852ee7cc8e310a408a13449cee58fdbabc1
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-04-05 21:07:02 -0400

    TEMP: contacts that have been called at least once are unavailable.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ApplicationRecord
   has_many :calls
 
   def available?
-    !checked_out_at || !(checked_out_at > 1.hour.ago)
+    (!checked_out_at || !(checked_out_at > 1.hour.ago)) && calls.count == 0
   end
 
   def check_in!
